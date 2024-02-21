511ab43d28ea684cdbc2b0b4b32fe3c64e67f42e
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-02-21 15:48:12 +0530

    setup folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main>
-      <h1 className={styles.h1}>
-        Partners
-      </h1>
+
     </main>
   );
 }
