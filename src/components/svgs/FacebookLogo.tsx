ecbc402d5ebf1633852e969315eb9a48acd3c212
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-02-22 14:59:38 +0530

    refactore: svg's for jsx & make: reusable

---
+++ src/components/svgs/FacebookLogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FacebookLogo() {
+    return (
+        <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clipPath="url(#clip0_0_1021)"> <path d="M18.425 31.375L18.4188 20.5438H22.0312L22.7188 16.0625H18.4188V13.1562C18.4188 11.925 19.025 10.7312 20.95 10.7312H22.9V6.91875C22.9 6.91875 21.1313 6.61875 19.4312 6.61875C15.8875 6.61875 13.575 8.7625 13.575 12.65V16.0625H9.6375V20.5438H13.575V31.375C6.16875 30.2125 0.5 23.8 0.5 16.0625C0.5 7.5 7.4375 0.5625 16 0.5625C24.5625 0.5625 31.5 7.5 31.5 16.0625C31.5 23.8 25.8313 30.2125 18.4188 31.375H18.425Z" fill="#BECBDA" /> </g> <defs> <clipPath id="clip0_0_1021"> <rect width="32" height="32" fill="white" transform="translate(0 0.0625)" /> </clipPath> </defs> </svg>
+    )
+}
+
+export default FacebookLogo
