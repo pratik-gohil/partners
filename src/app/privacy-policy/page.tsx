d9030ed99492ea9c22bc0b8320223b29b986f614
sheikhjaved20 <sheikh.javed20@gmail.com>
2024-02-28 12:14:57 +0530

    form modal

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,4 +1,5 @@
 import Breadcrumb from "../../components/breadcrumb/BreadcrumbSec";
+import PolicyNavigation from "@/components/policyNavigation/PolicyNavigation";
 import styles from "../../styles/Static.module.scss";
 
 
@@ -9,6 +10,24 @@ export default function PrivacyPolicy() {
       <section className={`${styles.staticSec}`}>
         <div className={`${styles.maContainer}`}>
           <h2 className={`${styles.secTitle}`}>Our Policies</h2>
+          {/* <PolicyNavigation /> */}
+          <div className={`${styles.mainworTc} ${styles.pb40}`}>
+            <h1 className={`${styles.title}`}>Privacy Policy</h1>
+            <p>
+              We at Mirae Asset Capital Markets (India) Private Limited, recognize the
+              importance of protecting your personal and financial information. We assure
+              that the information parted to us will kept in strict confidentiality and to
+              use the information only for to help us service your requirement, to improve
+              our services to you, to provide you with products you have requested, and to
+              inform you about additional products or services that may be of interest to
+              you.
+              <br />
+              <br />
+              This site contains links to other web sites,Mirae Asset Capital Markets
+              (India) Private Limited is not responsible for the privacy practices or the
+              content of such web sites.
+            </p>
+          </div>
         </div>        
       </section>
     </>
--- src/app/referral-terms-and-conditions/page.tsx
+++ src/app/referral-terms-and-conditions/page.tsx
@@ -1,4 +1,5 @@
 import Breadcrumb from "../../components/breadcrumb/BreadcrumbSec";
+import PolicyNavigation from "@/components/policyNavigation/PolicyNavigation";
 import styles from "../../styles/Static.module.scss";
 
 
@@ -9,7 +10,184 @@ export default function ReferralTermsAndConditions() {
       <section className={`${styles.staticSec}`}>
         <div className={`${styles.maContainer}`}>
           <h2 className={`${styles.secTitle}`}>Our Policies</h2>
-        </div>        
+          {/* <PolicyNavigation /> */}
+          <div className={`${styles.mainworTc} ${styles.pb40}`}>
+            <h1 className={`${styles.title}`}>Partner Referral T&amp;C</h1>
+            <ul className={`${styles.listpayout}`}>
+              <li>
+                I understand that any references shared by the clients mapped to a
+                particular Authorised Person will be shared with the respective Authorised
+                Person on the best-case scenario and should be treated as an additional
+                support and not obligation on m.stock to pass such references to the
+                Authorised Partners.
+              </li>
+              <li>
+                The existing scheme of Refer and Earn run by m.Stock for its clients does
+                not apply to the clients mapped under Authorised Person.
+              </li>
+              <li>
+                The Authorised Person shall not run any Multi-level Marketing Scheme (MLM)
+                or any other form of such scheme to promote the referral program to gain
+                references from the clients mapped under their name /code or with any
+                prospect clients under any circumstances.
+              </li>
+              <li>
+                The Authorised person shall not publish the refer and earn scheme on
+                social media platforms, campaigns, promotional activities online or
+                offline or in any other form without prior written approval from Mirae
+                Asset Partners/m.Stock.
+              </li>
+              <li>
+                The sole intention of the Referral program is to get references of the
+                friends and family from the existing clients of m.Stock.
+              </li>
+              <li>
+                Authorised Person shall maintain full confidentiality of the references
+                given by their clients and shall not share these details with any third
+                party.
+              </li>
+              <li>
+                The Authorised person understands and agree that in case of any references
+                that are shared by the referrer is an existing client of m.Stock ,
+                Authorised Partner will not be able to open the new account for this
+                client, neither m.Stock has an obligation to transfer such clients to the
+                Authorised Partner and map it under his code.
+              </li>
+              <li>
+                You agree that any references given by the Referrer will be solely with
+                intention of introducing their friends and family members to the
+                respective Authorized person and no commercial transactions are intended
+                for the given references and below points are taken into consideration.
+                <ul className={`${styles.insidebarulr}`}>
+                  <li>
+                    Spam, bulk distribution of invitations, distribution to strangers or
+                    any other promotion of m.Stock referral program offer through
+                    invitations that would constitute or appear to constitute unsolicited
+                    commercial e-mail, SMS, publishing any article or content about
+                    m.Stock products, services etc.
+                  </li>
+                  <li>
+                    Advertisement of any kind in any form on any medium print, audio
+                    visual form, web etc. including social media (Facebook, Twitter,
+                    WhatsApp, Instagram, YouTube etc.), or spam under any applicable law
+                    or regulation.
+                  </li>
+                  <li>
+                    Fraud, attempted fraud or abuse of the m.Stock Refer and Earn offer or
+                    these Terms &amp; Conditions.
+                  </li>
+                  <li>
+                    Organising competition, contests online, posting content about m.Stock
+                    etc.
+                  </li>
+                  <li>
+                    Any act or omission resulting in damage to m.Stock in any form
+                    whatsoever in nature.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <div className={`${styles.title}`}>
+              Terms and Conditions for Authorised Person Clients
+            </div>
+            <ul className={`${styles.listpayout}`}>
+              <li>
+                The Refer and Earn program of m.Stock is currently applicable for direct
+                clients only.
+              </li>
+              <li>
+                You agree that the details like name, mobile number, email id for the lead
+                / reference shared by client registered through Authorized Person will be
+                shared to the respective Authorized Person with referral details.
+              </li>
+              <li>
+                m.Stock will not be responsible for any referral incentives / schemes
+                agreed between Referrer and the Authorized Person.
+              </li>
+              <li>
+                Any grievances related to above scheme / offer / campaign would not be
+                entertained under investor protection or grievance redressal mechanism of
+                the Exchange.
+              </li>
+              <li>
+                You agree that any references given by the Referrer will be solely with
+                intention of introducing your friends and family members to the respective
+                Authorized person and no commercial transactions are intended for the
+                given references and below points are taken into consideration.
+              </li>
+              <li>
+                You agree that the following activities are expressly prohibited
+                <ul className={`${styles.insidebarulr}`}>
+                  <li>
+                    Spam, bulk distribution of invitations, distribution to strangers or
+                    promotion of any other referral program offer through invitations that
+                    would constitute or appear to constitute unsolicited commercial
+                    e-mail, SMS, publishing any article or content about m.Stock products,
+                    services etc.
+                  </li>
+                  <li>
+                    Advertisement of any kind in any form on any medium print, audio
+                    visual form, web etc. including social media (Facebook, Twitter,
+                    WhatsApp, Instagram, YouTube etc.), or spam under any applicable law
+                    or regulation.
+                  </li>
+                  <li>
+                    Fraud, attempted fraud or abuse of the Refer and Earn program or these
+                    Terms &amp; Conditions.
+                  </li>
+                  <li id="partnerReferralTC">
+                    Organising competition, contests online, posting content about m.Stock
+                    etc.
+                  </li>
+                  <li>
+                    Any act or omission resulting in damage to m.Stock in any form
+                    whatsoever in nature.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                m.Stock and its affiliates accept no liabilities for any loss or damage of
+                any kind arising out of any actions taken in reliance thereon about
+                references shared by the Referrer to the respective Authorized Person.
+              </li>
+            </ul>
+            <div className={`${styles.title}`}>Terms and Conditions for Partner Referral Program</div>
+            <ul className={`${styles.listpayout}`}>
+              <li>
+                Reward of ₹6,000 per successful registration will be paid if the referee
+                uses the registered mobile number or official link (sent in communications
+                or available on the partner dashboard) to initiate registration and the
+                referrer is a registered partner
+              </li>
+              <li>
+                If the referrer is not a registered partner, reward of ₹2,000 will be paid
+                through an Amazon gift voucher. Once the referrer completes their partner
+                registration (anytime in the future), additional ₹4,000 per past
+                successful registration will be credited to your payout ledger
+              </li>
+              <li>
+                There is no limit on the reward or the number of people that can be
+                referred{" "}
+              </li>
+              <li>
+                If details of the referred business partner already exists in our
+                database, then it will not be considered for any referral rewards{" "}
+              </li>
+              <li>
+                Company reserves the right to amend this offer and these terms and
+                conditions at any time without prior notice. The decision of Mirae Asset
+                Partners on all matters relating to, or in connection with the use of this
+                offer shall be final and binding on all parties concerned
+              </li>
+              <li>
+                For rewards credited to the payout ledger, 5% TDS (Tax Deducted at Source)
+                will be deducted as per applicable Income Tax Law. Or 10% TDS will be
+                applicable if the referrer is a specified person under section 206AB
+              </li>
+            </ul>
+          </div>
+
+        </div>
       </section>
     </>
   );
