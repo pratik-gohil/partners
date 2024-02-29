637a5f09e696d0c23c99430b4307f622dcf3f045
raj-macm <kaibalya.neosoft@miraeassetcm.com>
2024-02-29 11:53:29 +0530

    dev changes

--- src/app/partner-referral-program/page.tsx
+++ src/app/partner-referral-program/page.tsx
@@ -1,7 +1,7 @@
 import Breadcrumb from "../../components/breadcrumb/BreadcrumbSec";
-import PartRefProBannerSec from "./PartRefProBannerSec/PartRefProBannerSec"; 
+import PartRefProBannerSec from "./PartRefProBannerSec/PartRefProBannerSec";
 import StartPartnerReferringSec from "./StartPartnerReferringSec/StartPartnerReferringSec";
-import BenefitsOfPartneringSec from "./BenefitsOfPartneringSec/BenefitsOfPartneringSec"; 
+import BenefitsOfPartneringSec from "./BenefitsOfPartneringSec/BenefitsOfPartneringSec";
 import EarnRewardsSec from "./EarnRewardsSec/EarnRewardsSec";
 import HowToReferSec from "./HowToReferSec/HowToReferSec";
 import FaqsSec from "./FaqsSec/FaqsSec";
@@ -10,11 +10,12 @@ import FaqsSec from "./FaqsSec/FaqsSec";
 export default function PartnerReferralProgramPage() {
   return (
     <>
-      <Breadcrumb /> 
-      <PartRefProBannerSec />  
+      <Breadcrumb />
+      <PartRefProBannerSec />
       <StartPartnerReferringSec />
+
       <BenefitsOfPartneringSec />
-      <EarnRewardsSec /> 
+      <EarnRewardsSec />
       <HowToReferSec />
       <FaqsSec />
     </>
