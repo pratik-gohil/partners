a4fb95712ba12f2e111cca08db39eaac6ed2937c
Parshuram101 <parshuram.pawar@miraeassetcm.com>
2024-02-29 09:08:54 +0530

    css chnages added

--- src/app/pricing/PricePayoutStructureSec/PricePayoutStructureSec.tsx
+++ src/app/pricing/PricePayoutStructureSec/PricePayoutStructureSec.tsx
@@ -109,35 +109,11 @@ function PricePayoutStructureSec() {
                         <b className={`${styles.orangeTxt}`}>Note:</b> Mirae Asset will debit a flat fee of ₹500 for every new client onboarded, regardless of the account opening fee chosen
                     </p>    
                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                
-
+ 
 
                 <div className={`${styles.tableStructureWrap}`}>
                     <div className={`${styles.tableTitleWrap} ${styles.flex}`}>
-                        <img src="https://www.miraeassetpartners.com/asset/images/rs-icon-1.png" alt="icon" title="icon" />
+                        <img src="https://www.miraeassetpartners.com/asset/images/incom-from-icon.png" alt="icon" title="icon" />
                         <h3 className={`${styles.tableTitle}`}>Income from Brokerage</h3>
                     </div>   
                     <p>You can either select from our <b>standard plans</b> or <b>create a customised plan</b> for your clients based on their differentiated needs and trading behaviour.</p> 
@@ -174,8 +150,135 @@ function PricePayoutStructureSec() {
                             </tr> 
                         </table>
                     </div>    
-                </div>
+                </div> 
+
                  
+                <div className={`${styles.tableStructureWrap}`}>
+                    <div className={`${styles.tableTitleWrap} ${styles.flex}`}>
+                        <img src="https://www.miraeassetpartners.com/asset/images/intrest-percent-icon.png" alt="icon" title="icon" />
+                        <h3 className={`${styles.tableTitle}`}>Income from MTF (eMargin) Interests</h3>
+                    </div>   
+                    <p>You can either select our standard interest rates, i.e. base interest rates or choose to customise the interest rates applicable to the client.</p> 
+                    <div className={`${styles.pricingTableHolder}`}>  
+                        <table width="100%">
+                            <tr> 
+                                <th>Base Interest Rate</th>
+                                <th>Payout to Partner <br/>(up to base rate) </th>
+                                <th>Customised interest 
+                                    rate <br/>(maximum limit)
+                                </th>
+                                <th>Payout to Partner
+                                    <br/>(above base rate)</th> 
+                            </tr>
+                            <tr> 
+                                <td>9.99%</td>
+                                <td className={`${styles.orangeTxt}`}><b className={`${styles.orangeTxt}`}>5%</b></td> 
+                                <td>24%</td>
+                                <td className={`${styles.orangeTxt}`}><b className={`${styles.orangeTxt}`}>80%</b></td> 
+                            </tr> 
+                        </table>
+                    </div> 
+                    <p><b className={`${styles.orangeTxt}`}>Note:</b> You can earn 5% by charging our standard MTF interest rates to your clients. However, if you want to charge your clients below 9.99% and up to 6.99% p.a there will be no sharing.</p>    
+                </div> 
+ 
+
+                <div className={`${styles.tableStructureWrap}`}>
+                    <div className={`${styles.tableTitleWrap} ${styles.flex}`}>
+                        <img src="https://www.miraeassetpartners.com/asset/images/doc-icon.png" alt="icon" title="icon" />
+                        <h3 className={`${styles.tableTitle}`}>Income from Margin Pledge Interests</h3>
+                    </div>   
+                    <p>You can either select our standard interest rates, i.e. base interest rates or choose to customise the interest rates applicable to the client. </p> 
+                    <div className={`${styles.pricingTableHolder}`}>  
+                    <table width="100%">
+                            <tr>
+                                <th>Margin Pledge 
+                                    Funding Value</th>
+                                <th>Base Interest Rate</th>
+                                <th>Payout to Partner 
+                                    <br />(up to base rate)
+                                </th>
+                                <th>Customised interest 
+                                    rate <br />(maximum limit)</th> 
+                                <th>Payout to Partner
+                                    <br />(above base rate)
+                                </th>
+                            </tr>
+                            <tr>
+                                <td>Up to 80% of 
+                                    pledged stocks 
+                                    value</td>
+                                <td>11.99%</td>
+                                <td className={`${styles.orangeTxt}`}><b className={`${styles.orangeTxt}`}>5%</b></td> 
+                                <td>24%</td>
+                                <td className={`${styles.orangeTxt}`}><b className={`${styles.orangeTxt}`}>80%</b></td> 
+                            </tr> 
+                        </table>
+                    </div>    
+                </div>
+ 
+
+                <div className={`${styles.tableStructureWrap}`}>
+                    <div className={`${styles.tableTitleWrap} ${styles.flex}`}>
+                        <img src="https://www.miraeassetpartners.com/asset/images/amc-pricing-icon.png" alt="icon" title="icon" />
+                        <h3 className={`${styles.tableTitle}`}>Income from Account Maintenance Charges (DP AMC)</h3>
+                    </div>   
+                    <p>You can either select from our standard DP AMC plans or customise client’s DP AMC charges. </p> 
+                    <div className={`${styles.pricingTableHolder}`}>  
+                    <table width="100%">
+                        <thead>
+                            <tr>
+                                <th colSpan={3} className={`${styles.orangeTxt}`}><span>Standard AMC Plans</span></th>
+                                <th colSpan={3}><span>Customised AMC Plans</span></th>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <th width="20%">Standard Plan 1 <br />(one-time AMC)</th>
+                            <th width="20%">Standard Plan 2 <br />(Quarterly AMC)</th>
+                            <th width="13%">Payout to Partner</th> 
+                            <th width="13%">One-time AMC</th> 
+                            <th width="13%">Quarterly AMC</th>
+                            <th width="21%">Payout to Partner</th>
+                        </tr>
+                        <tr>
+                            <td>₹999 (Lifetime free AMC with a one-time fee)</td>
+                            <td>₹480 (Annual fee, charged every quarter @₹120)</td>
+                            <td>20%</td> 
+                            <td>₹1,299 to ₹4,999</td>
+                            <td>₹600 to ₹1,000</td>
+                            <td className={`${styles.orangeTxt}`}>
+                                <b>100% payout above</b>
+                                <ul>
+                                    <li>₹999 <br />(one-time AMC)</li>
+                                    <li>₹480  <br />(quarterly AMC)</li>
+                                </ul>
+                            </td>  
+                        </tr> 
+                    </table>
+                    </div>    
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                 
             </div>
