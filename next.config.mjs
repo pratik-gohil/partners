6b24e140222f3bf3741960700cedd0a8df716ff0
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-03-12 18:49:32 +0530

    runtime env test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    publicRuntimeConfig: {
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL
+    },
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL
 };
 
 export default nextConfig;
--- src/lib/http/http.ts
+++ src/lib/http/http.ts
@@ -1,8 +1,11 @@
+import config from "../../../next.config.mjs";
+
 function http(url: string | URL | Request, init?: RequestInit): Promise<Response> {
     return (
         typeof url === 'string' && url.startsWith('/')
-            ? fetch(process.env.NEXT_PUBLIC_BASE_URL + url, init)
-            : fetch(url, init)
+            ? fetch(config.baseUrl + url, init)
+            : 
+            fetch(url, init)
     )
 }
 
