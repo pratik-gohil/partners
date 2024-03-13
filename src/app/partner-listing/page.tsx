5cc07fe8f9a68529f9cdd4ddc7751be5d94b2a8c
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-03-13 15:30:34 +0530

    move pagination to table component

--- src/app/partner-listing/page.tsx
+++ src/app/partner-listing/page.tsx
@@ -2,11 +2,9 @@
 import ListOfAuthorisedPersonsSec from "./ListOfAuthorisedPersonsSec/ListOfAuthorisedPersonsSec";
 import AccountOfDisciplinaryReasonsSec from "./AccountOfDisciplinaryReasonsSec/AccountOfDisciplinaryReasonsSec";
 
-
 export default function PartnerListingPage() {
   return (
     <>
-
       <ListOfAuthorisedPersonsSec />
       <AccountOfDisciplinaryReasonsSec />
     </>
