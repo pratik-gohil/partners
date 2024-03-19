a74902098e9442f6ae20f505bafc86de9bd16edd
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-03-19 15:47:32 +0530

    revert: gtm changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import styles from "./maCommanStyle.module.scss";
 import { PreloadResources } from "./preload-resources";
 import dynamic from "next/dynamic";
 import BreadcrumbSec from "@/components/breadcrumb/BreadcrumbSec";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export let commonMetadata = {
   openGraph: {
@@ -92,22 +92,7 @@ export default function RootLayout({
         <Footer />
         <div id='modal-portal' />
       </body>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-NTX7SZZG"
-        strategy="beforeInteractive"
-      />
-      <Script
-        id="gtm-script"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', "GTM-NTX7SZZG");
-          `,
-        }}
-      />
+      <GoogleTagManager gtmId="GTM-NTX7SZZG" />
     </html>
   );
 }
