ed2eed6de4927b83e0c61631a6bc8cc5aed6b9d4
Parshuram101 <parshuram.pawar@miraeassetcm.com>
2024-03-21 12:11:23 +0530

    html/css changes added - Pricing CTA

---
+++ src/app/pricing-cta/page.tsx
@@ -0,0 +1,20 @@
+import PricingCTABannerSec from "./PricingCTABannerSec/PricingCTABannerSec";
+import { getMetaData } from "../layout";
+
+export async function generateMetadata() {
+  const commonMetadata = getMetaData({
+    title: 'Sub Broker Commission Structure - Sub Brokers Income',
+    description: 'Interested in becoming a sub-broker or authorized partner? Explore the commission structure for sub-brokers at Mirae Asset Partners.',
+    keywords: ['sub broker income', ' sub broker commission structure', 'sub broker earnings']
+  }, "pricing CTA");
+
+  return commonMetadata;
+}
+
+export default function PricingCTA() {
+  return (
+    <>
+      <PricingCTABannerSec />
+    </>
+  );
+}
