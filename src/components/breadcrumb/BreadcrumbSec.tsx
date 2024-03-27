cdb641228eeff64581e44228651a888422ac8ea1
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-03-27 12:07:07 +0530

    add: nested breadcumb

--- src/components/breadcrumb/BreadcrumbSec.tsx
+++ src/components/breadcrumb/BreadcrumbSec.tsx
@@ -15,13 +15,13 @@ const data = {
     "/faq": "Partner FAQs",
     "/sitemap": "Sitemap",
     "/downloads": "Downloads",
-    "/partnership-terms-of-use": "Terms of Use",
-    "/privacy": "Privacy Policy",
-    "/partnership-terms-and-conditions": "Terms and Conditions",
-    "/investor-advisory": "Advisory For Investors",
-    "/risk-disclaimer": "Disclaimer",
-    "/refer-and-earn-terms-and-conditions": "Referral Terms and Conditions",
-    "/mtf-emargin-terms-and-conditions": "Emargin Terms and Conditions"
+    "/partnership-terms-of-use": ["Our Policy", "Terms of Use"],
+    "/privacy": ["Our Policy", "Privacy Policy"],
+    "/partnership-terms-and-conditions": ["Our Policy", "Terms and Conditions"],
+    "/investor-advisory": ["Our Policy", "Advisory For Investors"],
+    "/risk-disclaimer": ["Our Policy", "Disclaimer"],
+    "/refer-and-earn-terms-and-conditions": ["Our Policy", "Referral Terms and Conditions"],
+    "/mtf-emargin-terms-and-conditions": ["Our Policy", "Emargin Terms and Conditions"]
 }
 
 const BreadcrumbSec = () => {
@@ -37,7 +37,12 @@ const BreadcrumbSec = () => {
                 <div className={`${styles.maContainer}`}>
                     <ul>
                         <li><RetainQueryLink href="/"><Image src="/icon-home.svg" width={15} height={15} alt="home" title="home" /></RetainQueryLink></li>
-                        <li>{innerPage}</li>
+                        {typeof innerPage === "string" ?
+                            <li>{innerPage}</li>
+                            : innerPage.map((i: string) =>
+                                <li key={i}>{i}</li>
+                            )
+                        }
                     </ul>
                 </div>
             </div>)
