321ee908c46f99b6dcd308e426ea4f66f5433f92
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-03-28 18:38:58 +0530

    preloading banner images

--- src/app/[lang]/partner-referral-program/PartRefProBannerSec/PartRefProBannerSec.tsx
+++ src/app/[lang]/partner-referral-program/PartRefProBannerSec/PartRefProBannerSec.tsx
@@ -12,12 +12,12 @@ function PartRefProBannerSec() {
                         <span>Refer a partner & share this</span>
                         <b className={`${styles.b1}`}>unbeatable</b>
                         <span>business</span>
-                        <b className={`${styles.b2}`}>opport<Image src="/referrals/text-U-img.webp" alt='opportunity…' width={45} height={45} quality={100} />&nbsp;nity…</b>
+                        <b className={`${styles.b2}`}>opport<Image priority src="/referrals/text-U-img.webp" alt='opportunity…' width={45} height={45} quality={100} />&nbsp;nity…</b>
                     </h2>
                     <div className={`${styles.priceBandStrip}`}>₹6,000 per successful registration!</div>
                 </div>
                 <div className={`${styles.rhsWrap}`}>
-                    <Image src="/referrals/banner-img.webp" alt='Refer a partner & share this UNBEATABLE business OPPORTUNITY…' width={500} height={500} quality={100} />
+                    <Image priority src="/referrals/banner-img.webp" alt='Refer a partner & share this UNBEATABLE business OPPORTUNITY…' width={500} height={500} quality={100} />
                 </div>
             </div>
         </section>
