9faaec2b669c7eebe5b411152a91526d0ffcca34
pratik-mirae <155417447+pratik-mirae@users.noreply.github.com>
2024-04-01 13:14:58 +0530

    fix: apos

--- src/app/[lang]/preload-resources.tsx
+++ src/app/[lang]/preload-resources.tsx
@@ -4,22 +4,21 @@ import ReactDOM from 'react-dom'
 
 export function PreloadResources() {
     ReactDOM.preconnect('https://www.miraeassetpartners.com')
-    ReactDOM.prefetchDNS('https://www.miraeassetpartners.com'
-
-    )
-    ReactDOM.preconnect('https://www.googletagmanager.com')
-    ReactDOM.prefetchDNS('https://www.googletagmanager.com')
-
-    ReactDOM.preconnect('https://www.google-analytics.com')
-    ReactDOM.prefetchDNS('https://www.google-analytics.com')
-
-    ReactDOM.preload("https://www.googletagmanager.com/gtm.js?id=GTM-NTX7SZZG", {
-        as: "script"
-    })
+    ReactDOM.prefetchDNS('https://www.miraeassetpartners.com')
 
     ReactDOM.preload("/OpenSans.woff2", {
         as: "font"
     })
+
+    // ReactDOM.preconnect('https://www.googletagmanager.com')
+    // ReactDOM.prefetchDNS('https://www.googletagmanager.com')
+
+    // ReactDOM.preconnect('https://www.google-analytics.com')
+    // ReactDOM.prefetchDNS('https://www.google-analytics.com')
+
+    // ReactDOM.preload("https://www.googletagmanager.com/gtm.js?id=GTM-NTX7SZZG", {
+    //     as: "script"
+    // })
 
     return null
 }
